61edc8601d7035b2ce93660826e3b7558bb4f4f6
Santiago <123smj123@gmail.com>
2026-02-25 18:06:13 +0100

    samples/SMJ

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ let app=express();
 app.use("/", express.static("./public"));
 
 const cool = require('./cool/cool.js');
+const indexSMJ = require('./samples/index-SMJ.js');
 const PORT = 3000;
 
 app.get('/', (req, res) => {
@@ -14,6 +15,10 @@ app.get('/', (req, res) => {
 app.get('/cool', (req, res) => {
   res.send(cool.coolFace());
 });
+
+app.get('/samples/SMJ', (req, res) => {
+  res.send(indexSMJ.calculateAverageSMJ('afghanistan', 'unsafe_water_source'));
+})
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
--- samples/index-SMJ.js
+++ samples/index-SMJ.js
@@ -1,5 +1,4 @@
 const xlsx = require('xlsx');
-
 const workbook = xlsx.readFile('data/SOS2526-10-Propuesta.xlsx');
 
 const sheetName = workbook.SheetNames[3]; //Uso la tercera hoja porque es la que tiene mis datos
@@ -7,6 +6,7 @@ const sheet = workbook.Sheets[sheetName];
 
 const data = xlsx.utils.sheet_to_json(sheet);
 
+/*
 const selectedEntity = 'afghanistan';
 const field = 'unsafe_water_source';
 
@@ -17,3 +17,21 @@ const values = filteredData.map(row => row[field]);
 const average = values.reduce((sum, value) => sum + value, 0) / values.length;
 
 console.log(`Average ${field} for ${selectedEntity}: ${average}`);
+*/
+
+function getAverage(entity, field) {
+  let values = data.filter(row => row.entity.toLowerCase() === entity.toLowerCase())
+    .map(row => row[field]);
+  return values.reduce((sum, value) => sum + value, 0) / values.length;
+}
+
+function calculateAverageSMJ(entity, field) {
+    const average = getAverage(entity, field);
+    return `<html><body><h1>
+        Average ${field} for ${entity}: ${average}
+        </h1></body></html>`;
+}
+
+module.exports = {
+    calculateAverageSMJ
+};
