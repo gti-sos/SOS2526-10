4ea36ea7f123c385fb42861bc422b5020bb78f55
Santiago <123smj123@gmail.com>
2026-02-25 18:27:28 +0100

    endpoints

---
+++ data/readFile.js
@@ -0,0 +1,12 @@
+const xlsx = require('xlsx');
+
+function readFile(fileName) {
+    let workbook = xlsx.readFile('data/datasets/' + fileName);
+    let sheetName = workbook.SheetNames[0];
+    let sheet = workbook.Sheets[sheetName];
+    return xlsx.utils.sheet_to_json(sheet);
+}
+
+module.exports = {
+    readFile
+};
--- index.js
+++ index.js
@@ -6,6 +6,7 @@ app.use("/", express.static("./public"));
 
 const cool = require('./cool/cool.js');
 const indexSMJ = require('./samples/index-SMJ.js');
+const fileReader = require('./data/readFile.js');
 const PORT = 3000;
 
 app.get('/', (req, res) => {
@@ -19,6 +20,20 @@ app.get('/cool', (req, res) => {
 app.get('/samples/SMJ', (req, res) => {
   res.send(indexSMJ.calculateAverageSMJ('afghanistan', 'unsafe_water_source'));
 })
+
+let deathByRiskFactor = []
+
+app.get('/deaths-by-risk-factors', (req, res) => {
+  res.json(deathByRiskFactor);
+});
+
+app.get('/deaths-by-risk-factors/loadInitialData', (req, res) => {
+  if (deathByRiskFactor.length === 0) {
+    deathByRiskFactor = fileReader.readFile('number-of-deaths-by-risk-factor.csv')
+      .slice(0, 10);
+  }
+  res.json(deathByRiskFactor);
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
